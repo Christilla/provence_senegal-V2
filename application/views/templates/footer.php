0677e6f023c39a2a8521cb84c0cf7969e6d79930
Christilla <cpuyjarinet@gmail.com>
2018-10-27 17:18:12 +0200

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,88 +3,96 @@
 </body>
 
 <!-- Footer -->
-<footer class="page-footer font-small blue pt-4" style="background-image: linear-gradient(to right, #f83600 0%, #f9d423 100%);">
-
+<footer class="page-footer font-small blue pt-4" style="background-image: linear-gradient(to top, #E94E1B , #F2C900)">
+  
+        <img class="img-fluid" src="assets/img/frise2.png" alt="frise" />
 
     <!-- Footer Links -->
     <div class="container-fluid text-center text-md-left">
-
+<h1 class="text-uppercase text-center text-light font-weight-bold">Provence Sénégal</h1>
       <!-- Grid row -->
-      <div class="row">
+      <div class="row justify-content-center">
 
         <!-- Grid column -->
-        <div class="col-md-6 mt-md-0 mt-3">
+        <!-- <div class="col-md-6 mt-md-0 mt-3">
 
           <!-- Content -->
-          <h5 class="text-uppercase">Footer Content</h5>
-          <p>Here you can use rows and columns here to organize your footer content.</p>
+          
+          <!-- <img class="img-fluid d-xl-block d-none " src="assets/img/decoratif.png" alt="symbole">
 
-        </div>
+        </div> -->
         <!-- Grid column -->
 
-        <hr class="clearfix w-100 d-md-none pb-3">
+          <div class="col-md-3">
+          <h5 class="text-uppercase">Accueil</h5>
 
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Actualités</a>
+            </li>
+            <li>
+              <a href="#!">Contact</a>
+            </li>
+          
+          </div>
         <!-- Grid column -->
-        <div class="col-md-3 mb-md-0 mb-3">
+        <div class="col-md-3">
 
             <!-- Links -->
-            <h5 class="text-uppercase text-light">Links</h5>
+            <h5 class="text-uppercase text-light-center">Volet Social</h5>
 
             <ul class="list-unstyled">
               <li>
-                <a href="#!">Link 1</a>
+                <a href="#!">Aide aux femmes</a>
               </li>
               <li>
-                <a href="#!">Link 2</a>
+                <a href="#!">Aide aux hopitaux</a>
               </li>
-              <li>
-                <a href="#!">Link 3</a>
-              </li>
-              <li>
-                <a href="#!">Link 4</a>
-              </li>
+              
             </ul>
 
           </div>
           <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-3 mb-md-0 mb-3">
+          <!-- Grid column --> 
+          <div class="col-md-3">
 
             <!-- Links -->
-            <h5 class="text-uppercase">Links</h5>
+            <h5 class="text-uppercase">Volet culturel</h5>
 
             <ul class="list-unstyled">
               <li>
-                <a href="#!">Link 1</a>
+                <a href="#!">Tourisme solidaire</a>
               </li>
               <li>
-                <a href="#!">Link 2</a>
+                <a href="#!">Centres de vacances</a>
               </li>
-              <li>
-                <a href="#!">Link 3</a>
-              </li>
-              <li>
-                <a href="#!">Link 4</a>
-              </li>
+             
             </ul>
 
           </div>
           <!-- Grid column -->
+          <div class="col-md-4">
+
+<!-- Links -->
+
+</ul>
+
+</div>
 
       </div>
       <!-- Grid row -->
 
     </div>
     <!-- Footer Links -->
-
+</footer>
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2018 Copyright:
-      <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
+    <div class="footer-copyright text-center py-2 bg-primary text-light">
+      © 2018 Made by Christilla MUSSARD PUYJARINET
     </div>
     <!-- Copyright -->
 
-  </footer>
+  
   <!-- Footer -->
 
 
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,6 @@
 <html>
 <meta charset="utf-8" lang="fr">
-    </head>
+    <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
@@ -8,19 +8,26 @@
     
        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" /> 
         <link rel="stylesheet" type="text/css" href="assets/js/bootstrap.js" /> 
+        <link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+        <link rel="stylesheet" type="text/css" href="assets/font">
+        <link href="https://fonts.googleapis.com/css?family=Trade+Winds" rel="stylesheet"> 
+       <!--  <style>
+            @import url('https://fonts.googleapis.com/css?family=Trade+Winds');
+        </style>  -->
        
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- <!--   <script src="assets/js/test.js"></script>  -->
+
      </head>
 
 <body>
+
 <div class="row justify-content-md-center" style="background-image: linear-gradient(to top, #E94E1B , #F2C900);">
   <div class="col col-12 col-md-10">
     <img class="img-fluid d-xl-block d-none " src="assets/img/logo.png" alt="logo">
-    
-      <h1 class="text-light font-weight-bold">PROVENCE SENEGAL</h1>
+    <!-- <h1 style="font-family: 'Trade Winds', cursive;" class="font-weight-bold">Provence Sénégal</h1> -->
+      <h1 class="font-weight-bold">Provence Sénégal</h1>
   </div> 
 </div>
 
@@ -47,15 +54,10 @@
       </li>
 
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+  
 
   </div>
 </nav>
-
-
 
 
 <div class="container-fluid"> 
